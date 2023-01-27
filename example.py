f92d02c29a02859f8a59f998f2b0697d585bbddd
Adithya Yerramsetty <adithyay@hotmail.com>
2023-01-26 19:32:03 -0700

    Version 1.0.0

---
+++ example.py
@@ -0,0 +1,17 @@
+from simplepycli import PyCLIApp
+
+app = PyCLIApp()
+
+
+@app.command("egg", "Prints egg to the console")
+def egg(params):
+    print("egg")
+
+
+@app.command("echotwice", "Echos the string passed in as a paramater twice")
+def echotwice(params):
+    for i in range(2):
+        print(params)
+
+
+app.run()
